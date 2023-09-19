e9da46e14f1483668cd7461401138d95b9b1e6d7
Your Name~Rian <you@example.comrian_beckert@hotmail.com>
2023-09-19 17:22:35 -0300

    lp cafeteria

--- src/UI/Stars.tsx
+++ src/UI/Stars.tsx
@@ -3,16 +3,17 @@ import styles from './Stars.module.scss';
 
 interface StarsProps {
   stars: number;
-  reviews: number;
+
+  
 }
 
-export function Stars({ stars, reviews }: StarsProps) {
+export function Stars({ stars }: StarsProps) {
   const renderStars = Array.from({ length: stars }, (_, i) => (
     <Image key={i} src='/star.svg' alt='estrela' width={14} height={14} />
   ));
 
   return <div className={styles.stars}>
     {renderStars}
-    {reviews && <span>({reviews})</span>}
+    
     </div>;
-}
+    }
